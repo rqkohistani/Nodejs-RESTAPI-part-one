ad7cad562b80076982d69b87877731dca1827e5f
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-13 00:36:23 +0200

    adds user data

---
+++ modules/user/user.data.js
@@ -0,0 +1,15 @@
+exports.userData = [
+  {
+    id: 1,
+    name: 'John',
+    password: 'password',
+    hashedPassword: null,
+  },
+  {
+    id: 2,
+    name: 'Sally',
+    password: 'password',
+    hashedPassword: null,
+  },
+];
+module.exports = exports.userData;
