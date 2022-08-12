c068781f7c09255210c73411ce87b7077f49b205
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-13 00:39:08 +0200

    adds get a user route

--- server.js
+++ server.js
@@ -15,6 +15,34 @@ app.get('/', (req, res) => {
 app.get('/api/users', (req, res) => {
   res.json(userData);
 });
+app.get('/api/users/:id', (req, res) => {
+  try {
+    const id = Number(req.params.id);
+    const user = userData.find((user) => user.id === id);
+    if (!user) {
+      res.status(404).send('User not found');
+    } else {
+      console.log(user);
+      res.json(user);
+    }
+  } catch (err) {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
+    // const { id } = req.params;
+    const id = Number(req.params.id);
+    // const id2 = 1;
+    const user = userData.find((user) => user.id === id);
+    if (!user) {
+      res.status(404).send('User not found');
+    } else {
+      console.log(user);
+      res.json(user);
+    }
+  } catch (err) {
+    res.status(404).json({ message: 'User not found' });
+  }
+});
 });
 // start the server
 app.listen(port, () => {
