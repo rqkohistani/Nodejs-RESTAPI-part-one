0240797b19e74d0e482c0a4bbb0ede81e3cb0b02
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-14 10:36:15 +0200

    implements getAllUsers and getUser

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,1 +1,22 @@
 import defaultData from './user.default.data';
+
+const getAllUsers = () => {
+  return defaultData.userData;
+};
+
+const getUser = (id) => {
+  const user = defaultData.userData.find((user) => user.id === id);
+  if (!user) {
+    throw new Error('User not found user.service');
+  } else {
+    return user;
+  }
+}
+
+
+const userService = {
+  getAllUsers,
+  getUser,
+};
+
+export default userService;
