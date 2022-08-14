d609c78832d02bb6447ebaa769c84ab61d95a906
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-14 10:27:48 +0200

    adds user routes

--- routes.js
+++ routes.js
@@ -1,3 +1,18 @@
 import express from 'express';
 
 import userRoutes from './modules/user/user.routes';
+
+
+const routes = () => {
+  const router = express.Router();
+
+  router.use('/users', userRoutes());
+
+  return router;
+};
+
+export const notFoundRoute = (req, res) => {
+  return res.status(404).send('routeNotFoundMessage');
+};
+
+export default routes;
