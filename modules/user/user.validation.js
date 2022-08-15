74abe6896b631711eb3bad72a298c2d1132ccf4b
Rashed <rashidqk@gmail.com>
2022-08-15 14:41:25 +0200

    create user ajv schema validation fields

---
+++ modules/user/user.validation.js
@@ -0,0 +1,19 @@
+// TODO: add the validation to the user validation middleware
+// const createUserSchema = {
+//   "type": "object",
+//   "properties": {
+//     "id": {
+//       "type": "integer",
+//       "description": "The id of the user"
+//     },
+//     "name": { "type": "string", "minLength": 1 },
+//     "email": { "type": "string", "format": "email" },
+//     "password": { "type": "string", "minLength": 8 },
+//     "userRole": { "enum": ["admin", "user"] },
+//     "createdAt": { "type": "string", "format": "date-time" },
+//     "updatedAt": { "type": "string", "format": "date-time" }
+//   },
+//   "additionalProperties": false,
+//   "required": ["name", "email", "password", "userRole"]
+// }
+
