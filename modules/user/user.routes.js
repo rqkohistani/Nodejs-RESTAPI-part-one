ca271c6b11fa9554ac47e6dd49255d472df37160
Rashed <rashidqk@gmail.com>
2022-08-15 15:05:08 +0200

    validates create new user

--- modules/user/user.routes.js
+++ modules/user/user.routes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { getAllUsers, getUser, createUser, deleteUser, updateUser } from './user.controller';
+import userValidators from './user.validation';
 
 const routes = () => {
   const userRouter = express.Router();
 
   userRouter.get('/', getAllUsers);
   userRouter.get('/:id', getUser);
-  userRouter.post('/', createUser);
+  userRouter.post('/', userValidators.createUser,createUser);
   userRouter.delete('/:id', deleteUser);
   userRouter.patch('/:id', updateUser);
 
