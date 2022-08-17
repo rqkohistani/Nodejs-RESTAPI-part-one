076c41e1292b5f261344628e12a977a619aada75
Rashed <rashidqk@gmail.com>
2022-08-17 11:24:07 +0200

    adds auth router

--- routes.js
+++ routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
 import userRoutes from './modules/user/user.routes';
-
+import authRouter from './modules/auth/auth.routes';
 
 const routes = () => {
   const router = express.Router();
 
+  router.use('/auth', authRouter());
   router.use('/users', userRoutes());
 
   return router;
