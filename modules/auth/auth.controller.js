a5dedfee1e84638667bc7a8d37680d90b3084725
Rashed <rashidqk@gmail.com>
2022-08-17 14:38:15 +0200

    adds get User by email in controller

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -1,11 +1,13 @@
 import bcrypt from 'bcrypt';
 import { HttpError } from '../../errors';
 import authService from './auth.service';
-
+import userService, { getUserByEmail } from './../user/user.service';
+let currentUser = "";
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await authService.login(email, password);
+    currentUser = user;
     if (!user) throw new HttpError(404, 'User not found.');
     res.status(200).send(user);
   } catch (error) {
@@ -17,7 +19,24 @@ const logout = async (req, res, next) => {
   try {
     const { email } = req.body;
     const user = await authService.logout(email);
+    // currentUser = 'Loging please';
+    req.headers.authorization = '';
+    currentUser = '';
+
+    //  user = await authService.getUserFromAuthToken(req.headers?.authorization, email);
+
     if (!user) throw new HttpError(404, 'User not found.');
+    res.status(200).send(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getUserFromAuthToken = async (req, res, next) => {
+  try {
+     currentUser = currentUser?.email;
+    if (!currentUser) throw new HttpError(401, 'Unauthorized: Invalid token');
+    const user = await authService.getUserFromAuthToken(req.headers?.authorization, currentUser);
     res.status(200).send(user);
   } catch (error) {
     next(error);
@@ -27,8 +46,9 @@ const logout = async (req, res, next) => {
 const authController = {
   login,
   logout,
+  getUserFromAuthToken,
 };
 
 export default authController;
 
-export { login, logout };
+export { login, logout, getUserFromAuthToken };
