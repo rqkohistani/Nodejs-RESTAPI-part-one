a46b96a73bc5b9cd7935024fb26327b0404bff44
Rashed <rashidqk@gmail.com>
2022-08-17 14:38:01 +0200

    adds me route

--- modules/auth/auth.routes.js
+++ modules/auth/auth.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
-import { login, logout } from './auth.controller';
+import { login, logout, getUserFromAuthToken } from './auth.controller';
 import authValidators from './auth.validation';
-
 
 const routes = () => {
   const authRouter = express.Router();
 
-  authRouter.post('/login', authValidators.login,login);
-  authRouter.post('/logout', authValidators.logout,logout);
+  authRouter.post('/login', authValidators.login, login);
+  authRouter.post('/logout', authValidators.logout, logout);
+  authRouter.get('/me', getUserFromAuthToken);
 
   return authRouter;
 };
