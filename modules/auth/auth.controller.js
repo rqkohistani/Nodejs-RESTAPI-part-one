47a18828d6f5e19b1c864d17c878bc0c5328854e
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-19 08:15:05 +0200

    returns token if user is found

--- modules/auth/auth.controller.js
+++ modules/auth/auth.controller.js
@@ -1,13 +1,11 @@
-import bcrypt from 'bcrypt';
 import { HttpError } from '../../errors';
 import authService from './auth.service';
 
 const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const user = await authService.login(email, password);
-    if (!user) throw new HttpError(404, 'User not found.');
-    res.status(200).send(user);
+    const {token, user} = await authService.login(email, password);
+    res.status(201).send({token, user});
   } catch (error) {
     next(error);
   }
