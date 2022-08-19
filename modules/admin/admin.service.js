e7f21114e7aaf4ec73e0152eccd933815d34267d
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-19 22:36:15 +0200

    creates update admin func

--- modules/admin/admin.service.js
+++ modules/admin/admin.service.js
@@ -37,3 +37,30 @@ const deleteAdmin = (id) => {
   }
   return admin;
 };
+
+// FIXME: It does update the user but it does not return to the user the updated user in postman body response.
+// FIXME: if user is not found, it returns {}- in postman body response.
+const updateAdmin = async (id, newAdmin) => {
+  const oldAdmin = defaultData.user.find((admin) => admin.id === id);
+  if (oldAdmin) {
+    const data = fs.readFileSync('./dataBaseJson/default.data.json');
+    const admins = JSON.parse(data);
+    const newAdminData = admins.user.map((admin) => {
+      if (admin.id === id) {
+        return {
+          id: admin.id,
+          ...admin,
+          ...newAdmin,
+          password: bcrypt.hashSync(admin.password, 10),
+          updatedAt: new Date().toISOString(),
+        };
+      }
+      return admin;
+    });
+    fs.writeFileSync(
+      './dataBaseJson/default.data.json',
+      JSON.stringify({ user: newAdminData, userData: [...defaultData.userData] })
+    );
+    return oldAdmin;
+  }
+};
