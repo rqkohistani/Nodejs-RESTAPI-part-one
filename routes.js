5c1db004fe9874a61cfc88e80c085f5775be8455
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-19 22:50:26 +0200

    adds admin Router

--- routes.js
+++ routes.js
@@ -2,12 +2,14 @@ import express from 'express';
 
 import userRoutes from './modules/user/user.routes';
 import authRouter from './modules/auth/auth.routes';
+import adminRouter from './modules/admin/admin.routes';
 
 const routes = () => {
   const router = express.Router();
 
   router.use('/auth', authRouter());
   router.use('/users', userRoutes());
+  router.use('/admins', adminRouter());
 
   return router;
 };
