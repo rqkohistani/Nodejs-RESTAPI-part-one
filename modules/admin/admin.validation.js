453bfe0e6847b1bb7ec27230421df2eb795260f9
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-19 22:48:17 +0200

    validates admin delete schema

--- modules/admin/admin.validation.js
+++ modules/admin/admin.validation.js
@@ -1,5 +1,7 @@
 import { validateJsonSchema } from '../../utils/validation.util';
-import { createAdminSchema,updateAdminSchema } from './schemas';
+import { createAdminSchema,updateAdminSchema, deleteAdminSchema } from './schemas';
+import { HttpError } from '../../errors';
+
 const createAdmin = (req, res, next) => {
   const admin = {
     ...req.body,
@@ -20,6 +22,31 @@ const updateAdmin = (req, res, next) => {
   req.body = admin;
   next();
 };
+
+const deleteAdmin = (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  const secret = req.body.secret;
+  if (secretProvided(secret)) {
+    const admin = {
+      ...req.body,
+      id,
+    };
+    validateJsonSchema(deleteAdminSchema, admin);
+    req.body = admin;
+    next();
+  } else {
+    throw new HttpError(401, ` Unauthorized: Secret is not provided`);
+  }
+};
+
+const secretProvided = (secret) => {
+  if (secret === 'secret') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 export { createAdmin };
 
 const adminValidators = {
