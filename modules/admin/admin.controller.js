e31793bf35abc3a3886e3f36cb941580b1165ad3
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-19 22:52:04 +0200

    creates getAllAdmins func

--- modules/admin/admin.controller.js
+++ modules/admin/admin.controller.js
@@ -1,5 +1,15 @@
 import { HttpError } from '../../errors';
 import adminService from './admin.service';
+const getAllAdmins = (req, res, next) => {
+  try {
+    const admins = adminService.getAllAdmins();
+    if (!admins?.length) throw new HttpError(404, 'No admins found in the file');
+
+    res.status(200).send(admins);
+  } catch (error) {
+    next(error);
+  }
+};
 const createAdmin = (req, res, next) => {
   try {
     const admin = adminService.createAdmin(req.body);
