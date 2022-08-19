0ab42b983a3261241055a86a90432d56b91a4bdf
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-19 22:35:21 +0200

    creates get admin func

--- modules/admin/admin.service.js
+++ modules/admin/admin.service.js
@@ -4,3 +4,9 @@ const getAllAdmins = () => {
   const admins = defaultData.user;
   return admins;
 };
+
+const getAdmin = (id) => {
+  const admin = defaultData.user.find((admin) => admin.id === id);
+  return admin;
+};
+
