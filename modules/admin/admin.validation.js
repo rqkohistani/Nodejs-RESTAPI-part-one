770b0c91a7326a21df854289b26aa5bc91934b3a
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-19 22:47:29 +0200

    validates admin update schema

--- modules/admin/admin.validation.js
+++ modules/admin/admin.validation.js
@@ -1,5 +1,5 @@
 import { validateJsonSchema } from '../../utils/validation.util';
-import { createAdminSchema } from './schemas';
+import { createAdminSchema,updateAdminSchema } from './schemas';
 const createAdmin = (req, res, next) => {
   const admin = {
     ...req.body,
@@ -9,10 +9,22 @@ const createAdmin = (req, res, next) => {
   next();
 };
 
+const updateAdmin = (req, res, next) => {
+  const id = parseInt(req.params.id, 10);
+  const admin = {
+    ...req.body,
+    id,
+  };
+  validateJsonSchema(updateAdminSchema, admin);
+  delete admin.id;
+  req.body = admin;
+  next();
+};
 export { createAdmin };
 
 const adminValidators = {
   createAdmin,
+  updateAdmin,
 };
 
 export default adminValidators;
