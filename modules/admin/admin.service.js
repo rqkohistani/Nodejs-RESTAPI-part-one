0249d9a11d95e3b7f348449c87497d827bfffd65
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-19 22:35:55 +0200

    creates delete admin func

--- modules/admin/admin.service.js
+++ modules/admin/admin.service.js
@@ -23,3 +23,17 @@ const createAdmin = (admin) => {
   fs.writeFileSync('./dataBaseJson/default.data.json', JSON.stringify({ ...users, user: [...users.user, newAdmin] }));
   return newAdmin;
 };
+
+const deleteAdmin = (id) => {
+  const admin = defaultData.user.find((admin) => admin.id === id);
+  if (admin) {
+    const data = fs.readFileSync('./dataBaseJson/default.data.json');
+    const admins = JSON.parse(data);
+    const newAdminData = admins.user.filter((admin) => admin.id !== id);
+    fs.writeFileSync(
+      './dataBaseJson/default.data.json',
+      JSON.stringify({ user: newAdminData, userData: [...defaultData.userData] })
+    );
+  }
+  return admin;
+};
