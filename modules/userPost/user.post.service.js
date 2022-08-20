9f8ae5bf6413c9fbd5e82e55fd3d63548a4edcdf
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-20 20:25:51 +0200

    delete a post by userId "params" & postId req.body

--- modules/userPost/user.post.service.js
+++ modules/userPost/user.post.service.js
@@ -9,6 +9,31 @@ const getPostByUserId = async (userId) => {
   return [];
 };
 
+const deletePost = async (userId, postId) => { 
+  const user = defaultData.userDataLists.find((user) => user.id === userId);
+  if (user) {
+    const data = fs.readFileSync('./dataBaseJson/default.data.json');
+    const users = JSON.parse(data);
+    const newUserData = users.userDataLists.map((user) => {
+      if (user.id === userId) {
+        const newPosts = user.posts.filter((post) => post.id !== postId);
+        return {
+          id: user.id,
+          ...user,
+          posts: newPosts,
+          updatedAt: new Date().toISOString(),
+        };
+      }
+      return user;
+    });
+    fs.writeFileSync(
+      './dataBaseJson/default.data.json',
+      JSON.stringify({ adminsAndUsersLists: [...defaultData.adminsAndUsersLists], userDataLists: newUserData })
+    );
+    return user;
+  }
+  return user;
+};
 const createPost = async (userId, postData) => {  
   const user = defaultData.userDataLists.find((user) => user.id === userId);
   if (user) {
