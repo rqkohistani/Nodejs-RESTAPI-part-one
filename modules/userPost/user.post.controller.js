2788c85a7f7ea32bf72cc133a77fad41109ea32f
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-20 21:19:14 +0200

    uses now faker and manuel body posts

--- modules/userPost/user.post.controller.js
+++ modules/userPost/user.post.controller.js
@@ -47,7 +47,7 @@ const createPost = async (req, res, next) => {
     const userId = parseInt(req.baseUrl.split('/')[4], 10); // get the user id from the url
     const { title, body } = req.body;
     const postData = { userId, title, body };
-    const post = await userPostService.createPost(userId, postData);
+    const post = await userPostService.createPostByFakerOrPostReqBody(userId, postData);
     if (!post) throw new HttpError(404, 'User not found.');
     return res.status(201).json(post);
   } catch (error) {
