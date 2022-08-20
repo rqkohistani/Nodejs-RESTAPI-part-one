03a053ba562e0dc18868f7aa47bae82fadcdb0d0
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-20 19:38:47 +0200

    adds userPostRouter

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import userRoutes from './modules/user/user.routes';
 import authRouter from './modules/auth/auth.routes';
 import adminRouter from './modules/admin/admin.routes';
+import userPostRouter from './modules/userPost/user.post.routes';
 
 const routes = () => {
   const router = express.Router();
@@ -10,6 +11,7 @@ const routes = () => {
   router.use('/auth', authRouter());
   router.use('/users', userRoutes());
   router.use('/admins', adminRouter());
+  router.use('/posts/:userId', userPostRouter());
 
   return router;
 };
