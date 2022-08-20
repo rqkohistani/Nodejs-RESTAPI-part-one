719669284f098120dd2d4edf03607712620360ba
Rashed Qazizada <rashed.qazizada@softhouse.se>
2022-08-20 07:32:07 +0200

    refactors admin service

--- modules/admin/admin.service.js
+++ modules/admin/admin.service.js
@@ -3,12 +3,12 @@ import bcrypt from 'bcrypt';
 import defaultData from '../../dataBaseJson/default.data.json';
 
 const getAllAdmins = () => {
-  const admins = defaultData.user;
+  const admins = defaultData.adminsAndUsersLists;
   return admins;
 };
 
 const getAdmin = (id) => {
-  const admin = defaultData.user.find((admin) => admin.id === id);
+  const admin = defaultData.adminsAndUsersLists.find((admin) => admin.id === id);
   return admin;
 };
 
@@ -22,19 +22,22 @@ const createAdmin = (admin) => {
   };
   const data = fs.readFileSync('./dataBaseJson/default.data.json');
   const users = JSON.parse(data);
-  fs.writeFileSync('./dataBaseJson/default.data.json', JSON.stringify({ ...users, user: [...users.user, newAdmin] }));
+  fs.writeFileSync(
+    './dataBaseJson/default.data.json',
+    JSON.stringify({ ...users, adminsAndUsersLists: [...users.adminsAndUsersLists, newAdmin] })
+  );
   return newAdmin;
 };
 
 const deleteAdmin = (id) => {
-  const admin = defaultData.user.find((admin) => admin.id === id);
+  const admin = defaultData.adminsAndUsersLists.find((admin) => admin.id === id);
   if (admin) {
     const data = fs.readFileSync('./dataBaseJson/default.data.json');
     const admins = JSON.parse(data);
-    const newAdminData = admins.user.filter((admin) => admin.id !== id);
+    const newAdminData = admins.adminsAndUsersLists.filter((admin) => admin.id !== id);
     fs.writeFileSync(
       './dataBaseJson/default.data.json',
-      JSON.stringify({ user: newAdminData, userData: [...defaultData.userData] })
+      JSON.stringify({ adminsAndUsersLists: newAdminData, userDataLists: [...defaultData.userDataLists] })
     );
   }
   return admin;
@@ -43,11 +46,11 @@ const deleteAdmin = (id) => {
 // FIXME: It does update the user but it does not return to the user the updated user in postman body response.
 // FIXME: if user is not found, it returns {}- in postman body response.
 const updateAdmin = async (id, newAdmin) => {
-  const oldAdmin = defaultData.user.find((admin) => admin.id === id);
+  const oldAdmin = defaultData.adminsAndUsersLists.find((admin) => admin.id === id);
   if (oldAdmin) {
     const data = fs.readFileSync('./dataBaseJson/default.data.json');
     const admins = JSON.parse(data);
-    const newAdminData = admins.user.map((admin) => {
+    const newAdminData = admins.adminsAndUsersLists.map((admin) => {
       if (admin.id === id) {
         return {
           id: admin.id,
@@ -61,7 +64,7 @@ const updateAdmin = async (id, newAdmin) => {
     });
     fs.writeFileSync(
       './dataBaseJson/default.data.json',
-      JSON.stringify({ user: newAdminData, userData: [...defaultData.userData] })
+      JSON.stringify({ adminsAndUsersLists: newAdminData, userDataLists: [...defaultData.userDataLists] })
     );
     return oldAdmin;
   }
